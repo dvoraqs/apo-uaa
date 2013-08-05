5727e0c771e106a95d6527b0f058127447bd996b
Dvorax <john.beaumier@gmail.com>
2013-08-04 18:31:45 -0800

    commit before refactoring events index view with new methods in events model

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,38 +3,34 @@
 class EventsController < ApplicationController
 
   before_filter :set_nav
-  before_filter :get_events, :only => [:index]
   before_filter :get_event, :only => [:show, :edit, :update, :destroy]
 
   def index
     # display a list of events
-
     @page = 'Events'
     @header = 'Upcoming Events'
+    @events = Event.order('start').all
   end
 
   def show
     # display a specific event
-
     @page = @header = @event.name
   end
 
   def edit
     # return an HTML form for editing a photo
-
-    @page = @header = 'Edit ' + @event.name
+    @page = 'Edit ' + @event.name
+    @header = @event.name
   end
 
   def new
     # return an HTML form for creating a new event
-
     @page = @header = 'New Event'
     @event = Event.new
   end
 
   def create
     # create a new event
-
     event = Event.new(params[:event])
     if event.save
       redirect_to event_path(event)
@@ -43,15 +39,14 @@ class EventsController < ApplicationController
 
   def update
     # update a specific event
-
-    # do stuff
-    redirect_to event_path(@event)
+    if @event.update_attributes(params[:event])
+      redirect_to event_path(@event)
+    end
   end
 
   def destroy
     # delete a specific event
-
-    # do stuff
+    @event.destroy
     redirect_to events_path
   end
 
@@ -59,35 +54,27 @@ class EventsController < ApplicationController
     @nav = 'Events'
   end
 
-  def get_events
-    @events = Event.order('start').all
-
-    # @events.each do |event|
-    #   start_date = DateTime.parse(event.start)
-    #   end_date = DateTime.parse(event.end)
-
-    #   while start_date.advance(:days => 1) < end_date
-    #     start_date = start_date.advance(:days => 1)
-    #     end_date = start_date.advance(:seconds => -1)
-        
-    #     # copy event
-    #     event_copy = Event.new
-    #     event_copy.id = event.id
-    #     event_copy.name = event.name
-    #     event_copy.start = start_date.strftime('%F %R')
-    #     event_copy.end = event.end
-    #     event_copy.summary = "Multi-day Event Continued from " + event.start
-    #     event_copy.location = event.location
-    #     event_copy.status = event.status
-    #     @events.push(event_copy)
-
-    #     # change end time for current event
-    #     event.end = end_date.strftime('%F %R')
-    #   end 
-    # end
-  end
-
   def get_event
     @event = Event.find(params[:id])
+  end
+
+  def google_calendar
+    require 'rubygems'
+    require 'google/api_client'
+    require 'yaml'
+
+    oauth_yaml = YAML.load_file('.google-api.yaml')
+    client = Google::APIClient.new
+    client.authorization.client_id = oauth_yaml["client_id"]
+    client.authorization.client_secret = oauth_yaml["client_secret"]
+    client.authorization.scope = oauth_yaml["scope"]
+    client.authorization.refresh_token = oauth_yaml["refresh_token"]
+    client.authorization.access_token = oauth_yaml["access_token"]
+
+    if client.authorization.refresh_token && client.authorization.expired?
+      client.authorization.fetch_access_token!
+    end
+
+    service = client.discovered_api('calendar', 'v3')
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,85 @@
 class Event < ActiveRecord::Base
+  attr_accessible :name, :start, :end, :location, :status, :summary
+
+  def start_date
+  	DateTime.parse(self[:start])
+  end
+
+  def start_date_short
+  	self.start_date.strftime('%m/%d ')
+  end
+
+  def start_year
+  	self.start_date.strftime('%Y')
+  end
+
+  def start_month_caps
+  	self.start_date.strftime('%^b')
+  end
+
+  def start_day
+  	self.start_date.strftime('%d')
+  end
+
+  def start_weekday
+  	self.start_date.strftime('%a ')
+  end
+
+  def start_weekday_caps
+  	self.start_date.strftime('%^a')
+  end
+
+  def start_time
+  	self.start_date.strftime('%l%P')
+  end
+
+  def start_time_long
+  	self.start_date.strftime('%l:%M%P')
+  end
+
+  def start_minute
+  	self.start_date.strftime('%M')
+  end
+
+  # all of the same for the end date
+
+  def end_date
+  	DateTime.parse(self[:end])
+  end
+
+  def end_date_short
+  	self.end_date.strftime('%m/%d ')
+  end
+
+  def end_year
+  	self.end_date.strftime('%Y')
+  end
+
+  def end_month_caps
+  	self.end_date.strftime('%^b')
+  end
+
+  def end_day
+  	self.end_date.strftime('%d')
+  end
+
+  def end_weekday
+  	self.end_date.strftime('%a ')
+  end
+
+  def end_weekday_caps
+  	self.end_date.strftime('%^a')
+  end
+
+  def end_time
+  	self.end_date.strftime('%l%P')
+  end
+
+  def end_time_long
+  	self.end_date.strftime('%l:%M%P')
+  end
+
+  def end_minute
+  	self.end_date.strftime('%M')
+  end
 end
